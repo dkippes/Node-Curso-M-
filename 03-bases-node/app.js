05f30f0ddc8d38b0a8e166d93a959efdb0686b6b
dkippes <kippes.diego@gmail.com>
2022-06-23 12:25:35 -0300

    linea de comandos ejercicio

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,10 +1,9 @@
-
-const {crearArchivo} = require('./helpers/multiplicar');
+const argv = require('./config/yargs');
+const {crearArchivo} = require('./multiplicar/multiplicar')
+require('colors/safe');
 
 console.clear();
 
-const base = 5;
-
-crearArchivo(base)
-    .then(archivo => console.log(archivo, 'creado'))
-    .catch(e => console.log(e));
+crearArchivo(argv.b, argv.l, argv.h)
+.then(nombreArchivo => console.log(nombreArchivo.rainbow, 'creado'))
+.catch(err => console.log(err))
