a992fb39cf0017c8c50f785b9a07e75c1964ba6e
dkippes <kippes.diego@gmail.com>
2022-07-11 23:18:54 -0300

    clima app

---
+++ Node Curso/Clima App/models/busquedas.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+
+class Busquedas {
+    historial = ['Tegucigalpa', 'Madrid', 'San Jose'];
+
+    constructor() {
+
+    }
+
+    get paramsMapbox() {
+        return {
+            'access_token': process.env.MAPBOX_KEY,
+            'limit': 5,
+            'language': 'es'
+        }
+    }
+
+    async ciudad(lugar = '') {
+        try {
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox
+            })
+            const resp = await instance.get();   
+            console.log(resp.data);
+            return [];
+        } catch (error) {
+            return [];
+        }        
+    }
+}
+
+module.exports = Busquedas;
--- Node Curso/Proyecto - Tareas por Hacer/helpers/inquirer.js
+++ Node Curso/Proyecto - Tareas por Hacer/helpers/inquirer.js
@@ -10,31 +10,31 @@ const preguntas = [
         choices: [
             {
                 value: '1',
-                name: `${'1'.green} Crear tarea`
+                name: `${'1.'.green} Crear tarea`
             },
             {
                 value: '2',
-                name: `${'2'.green} Listar tareas`
+                name: `${'2.'.green} Listar tareas`
             },
             {
                 value: '3',
-                name: `${'3'.green} Listar tareas completadas`
+                name: `${'3.'.green} Listar tareas completadas`
             },
             {
                 value: '4',
-                name: `${'4'.green} Listar tareas pendientes`
+                name: `${'4.'.green} Listar tareas pendientes`
             },
             {
                 value: '5',
-                name: `${'5'.green} Completar tarea(s)`
+                name: `${'5.'.green} Completar tarea(s)`
             },
             {
                 value: '6',
-                name: `${'6'.green} Borrar tarea`
+                name: `${'6.'.green} Borrar tarea`
             },
             {
                 value: '0',
-                name: `${'7'.green} Salir`
+                name: `${'7.'.green} Salir`
             },
     ]
     }
