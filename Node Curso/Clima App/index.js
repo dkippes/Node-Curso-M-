cb12b1f0b2a03b638e288bdd8670340ddfd82c03
dkippes <kippes.diego@gmail.com>
2022-07-13 00:48:52 -0300

    clima app finalizado

--- Node Curso/Clima App/index.js
+++ Node Curso/Clima App/index.js
@@ -1,4 +1,4 @@
-const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+const { leerInput, inquirerMenu, pausa, listarLugares } = require("./helpers/inquirer");
 const Busquedas = require("./models/busquedas");
 require('dotenv').config();
 
@@ -12,17 +12,28 @@ const main = async() => {
             case 1:
                 const termino = await leerInput('Ciudad: ');
                 const lugares = await busquedas.ciudad(termino);
-                console.log(lugares);
+                const id = await listarLugares(lugares);
+                if( id === '0') continue;
+            
+                const lugarSel = lugares.find(l => l.id === id);
+                busquedas.agregarHistorial(lugarSel.nombre);
+                const clima = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng);
 
+                console.clear();
                 console.log('\nInformacion de la ciudad\n'.green);
-                console.log('Ciudad: ');
-                console.log('Lat: ');
-                console.log('Lng: ');
-                console.log('Temperatura: ');
-                console.log('Minima: ');
-                console.log('Maxima: ');
+                console.log('Ciudad: ', lugarSel.nombre.green);
+                console.log('Lat: ', lugarSel.lat);
+                console.log('Lng: ', lugarSel.lng);
+                console.log('Temperatura: ', clima.temp);
+                console.log('Minima: ', clima.min);
+                console.log('Maxima: ', clima.max);
+                console.log('Como esta el clima? ', clima.desc.green);
                 break;
             case 2:
+                busquedas.historialCapitalizado.forEach((lugar, i) => {
+                    const idx = `${i + 1}.`.green;
+                    console.log(`${idx} ${lugar}`);
+                })
                 break;
         }
 
--- Node Curso/Clima App/models/busquedas.js
+++ Node Curso/Clima App/models/busquedas.js
@@ -1,11 +1,20 @@
 const axios = require('axios');
-
+const fs = require('fs');
 
 class Busquedas {
-    historial = ['Tegucigalpa', 'Madrid', 'San Jose'];
+    historial = [];
+    dbPath = './db/database.json'
 
     constructor() {
+        this.leerDB();
+    }
 
+    get historialCapitalizado() {
+        return this.historial.map(lugar => {
+            let palabras = lugar.split(' ');
+            palabras = palabras.map(p => p[0].toUpperCase() + p.substring(1));
+            return palabras.join(' ');
+        });
     }
 
     get paramsMapbox() {
@@ -16,16 +25,24 @@ class Busquedas {
         }
     }
 
+    get paramsWeather() {
+        return {
+            appid: process.env.OPENWEATHER_KEY,
+            units: 'metrics',
+            lang: 'es'
+        }
+    }
+
     async ciudad(lugar = '') {
         try {
             const instance = axios.create({
                 baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
                 params: this.paramsMapbox
             })
-            const resp = await instance.get();   
-            resp.data.features.map(lugar => ({
+            const resp = await instance.get();
+            return resp.data.features.map(lugar => ({
                 id: lugar.id,
-                nombre: lugar.places_name,
+                nombre: lugar.place_name,
                 lng: lugar.center[0],
                 lat: lugar.center[1]
             }));
@@ -33,6 +50,51 @@ class Busquedas {
         } catch (error) {
             return [];
         }        
+    }
+
+    async climaLugar(lat, lon) {
+        try {
+            const instance = axios.create({
+                baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+                params: {...this.paramsWeather, lat, lon}
+            })
+            const resp = await instance.get();
+            const { weather, main } = resp.data;
+            
+            return {
+                desc: weather[0].description,
+                min: main.temp_min,
+                max: main.temp_max,
+                temp: main.temp
+            }
+        } catch (error) {
+            return [];
+        }        
+    }
+
+    agregarHistorial(lugar = '') {
+        if(this.historial.includes(lugar.toLocaleLowerCase())) {
+            return;
+        }
+        this.historial = this.historial.splice(0,5);
+        
+        this.historial.unshift(lugar.toLocaleLowerCase());
+
+        this.guardarDB();
+    }
+
+    guardarDB() {
+        const payload = {
+            historial: this.historial
+        };
+        fs.writeFileSync(this.dbPath, JSON.stringify(payload));
+    }
+
+    leerDB() {
+        if(!fs.existsSync(this.dbPath)) return;
+        const info = fs.readFileSync(this.dbPath, { encoding: 'utf-8'});
+        const data = JSON.parse(info);
+        this.historial = data.historial;
     }
 }
 
